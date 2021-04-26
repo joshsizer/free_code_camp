97ce97b1852a892f20ec4321155ad47078f8c11e
Joshua Sizer <joshsizer@gmail.com>
2021-04-26 01:19:27 -0400

    Outlined for inventory_update project

---
+++ algorithms/inventory_update/inventory_update.js
@@ -0,0 +1,5 @@
+function updateInventory(arr1, arr2) {
+    return [[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]]
+}
+
+module.exports = updateInventory
